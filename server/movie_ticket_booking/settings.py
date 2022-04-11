07ff873d34ce4b159a92260247a38b38a0d5d025
karthi <karthicbe2405@gmail.com>
2022-04-12 00:47:54 +0530

    db configuration

--- server/movie_ticket_booking/settings.py
+++ server/movie_ticket_booking/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = 'movie_ticket_booking.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'movie_ticket_booking',  
+        'USER': 'root',  
+        'PASSWORD': '123',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
 }
 
 
